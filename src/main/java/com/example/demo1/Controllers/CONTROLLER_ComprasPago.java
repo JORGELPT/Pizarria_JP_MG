bc209c6e789e368e1d345be4158087749929d9a4
JORGELPT <jorgelpt@github.com>
2026-05-19 14:54:58 -0400

    CONTROLLER_ComprasPago

--- src/main/java/com/example/demo1/Controllers/CONTROLLER_ComprasPago.java
+++ src/main/java/com/example/demo1/Controllers/CONTROLLER_ComprasPago.java
@@ -1,7 +1,6 @@
 package com.example.demo1.Controllers;
 
 import com.example.demo1.Database.Conexion;
-import com.example.demo1.Utils.JasperUtil;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -314,21 +313,5 @@ public class CONTROLLER_ComprasPago {
             this.cantidad       = new SimpleStringProperty(cantidad);
             this.estado         = new SimpleStringProperty(estado);
         }
-    }
-
-    @FXML
-    public void FnExportarReporteCompras() {
-        JasperUtil.exportarPDF(
-                "/com/example/demo1/reportes/Reporte_Compras.jrxml",
-                "Reporte_Compras.pdf"
-        );
-    }
-
-    @FXML
-    public void FnExportarReporteComprasVentas() {
-        JasperUtil.exportarPDF(
-                "/com/example/demo1/reportes/Reporte_ComprasVentas.jrxml",
-                "Reporte_ComprasVentas.pdf"
-        );
     }
 }
